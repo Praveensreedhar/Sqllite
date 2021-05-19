050336ccbd0c62f474f6e70ede02858f4b64a57e
Praveen <49310101+Praveensreedhar@users.noreply.github.com>
2021-05-19 12:59:50 +0530

    Update simple_sqllite_connection.py

--- simple_sqllite_connection.py
+++ simple_sqllite_connection.py
@@ -1,3 +1,13 @@
 import sqlite
 try:
+  conn = sqlite3.connect('test1')
+  print "Opened SQL DB";
+except Exception as e:
+  print("Error during connection :",str(e))
+results = conn.execute("SELECT * FROM COMPANY")
+
+for row in results:
+  print row
+  
+conn.close()
   
